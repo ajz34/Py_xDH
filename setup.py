99a571bb0f3bea45c618b5ba31318a181f86e7b6
ajz34 <17110220038@fudan.edu.cn>
2019-09-17 13:34:04 +0800

    deployment: try another time on travis-ci...

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyxdh",
-    version="0.0.1",
+    version="v0.0.1",
     author="ajz34",
     author_email="17110220038@fudan.edu.cn",
     description="xDH Functional Derivatives Enpowered by Python",
-    long_description="",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ajz34/Py_xDH",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: GPL v3",
-        "Operating System :: Linux",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
     ],
     # https://stackoverflow.com/questions/1612733/including-non-python-files-with-setup-py
     package_data={'': ['Validation/*']},
