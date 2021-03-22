d98f2648555de104966413dae45335cfe9cf6706
ajz34 <ajz34@outlook.com>
2021-03-22 11:55:52 +0800

    refactor: missing __init__

--- pyxdh/DerivTwice/__init__.py
+++ pyxdh/DerivTwice/__init__.py
@@ -1,8 +1,6 @@
 __all__ = [
-    "DerivTwiceSCF", "DerivTwiceNCDFT",
-    "DerivTwiceMP2", "DerivTwiceXDH",
-    "HessSCF", "HessNCDFT",
-    "HessMP2", "HessXDH",
+    "DerivTwiceSCF", "DerivTwiceNCDFT", "DerivTwiceMP2", "DerivTwiceXDH",
+    "HessSCF", "HessNCDFT", "HessMP2", "HessXDH",
     "PolarSCF", "PolarNCDFT",
     "PolarMP2", "PolarXDH",
     "DipDerivSCF", "DipDerivNCDFT",
@@ -15,10 +13,8 @@ __all__ = [
     "PolarUMP2",
 ]
 
-from pyxdh.DerivTwice.deriv_twice_scf import DerivTwiceSCF, DerivTwiceNCDFT
-from pyxdh.DerivTwice.deriv_twice_mp2 import DerivTwiceMP2, DerivTwiceXDH
-from pyxdh.DerivTwice.hess_scf import HessSCF, HessNCDFT
-from pyxdh.DerivTwice.hess_mp2 import HessMP2, HessXDH
+from pyxdh.DerivTwice.deriv_twice_r import DerivTwiceSCF, DerivTwiceNCDFT, DerivTwiceMP2, DerivTwiceXDH
+from pyxdh.DerivTwice.hess_r import HessSCF, HessNCDFT, HessMP2, HessXDH
 from pyxdh.DerivTwice.polar_scf import PolarSCF, PolarNCDFT
 from pyxdh.DerivTwice.polar_mp2 import PolarMP2, PolarXDH
 from pyxdh.DerivTwice.dipderiv_scf import DipDerivSCF, DipDerivNCDFT
